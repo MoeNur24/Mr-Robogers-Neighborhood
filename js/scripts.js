4af39b91a11ce16834354f76dfef4d10ec12b883
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-17 23:35:45 -0800

    update JS

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function myNeighbor() {
+
+}
+
+$(document).ready(function() {
+  $("#form-Uno").submit(function(event){
+    event.preventDefault()
+    
+    const inputNumber = $("#number").val()
+  })
+})
