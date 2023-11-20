ad98d7b0885b92290332eb5e517e16d7fe25814d
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-19 22:24:18 -0800

    add a second condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
     //Bussiness Logic
 function my(neighbor) {
-const number = [];
+let number = [];
 for (let i =0; i <= neighbor; i++) {
 number.push(i.toString());
 if ((number[i]).match("3")) {
     number [i] = "Can you be my neighbor?"    
-}
+    } else if ((number[i]).match("2")) {
+    number [i] = ("Holla");    
+    }  
+    return number;
 }
 }
 
 $(document).ready(function() {
   $("#form-Uno").submit(function(event) {
-    const inputNumber = $("#number").val();
-   $("form-Uno").submit(function(event) {
     event.preventDefault();
     const inputNumber = parseInt($("#number").val());
     myNeighbor(inputtedNumber);
-   })
-  })
-})
+   });
+  });
