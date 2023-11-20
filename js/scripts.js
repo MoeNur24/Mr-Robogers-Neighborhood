1e03ce55c26343e8441fd5cc8f5c49ef6c872add
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-19 21:38:21 -0800

    add condition in JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@ function myNeighbor(num) {
 let array = [];
 for (let i =0; i <= num; i + 1) {
 
+array.push(i.toString());
+if ((number[i]).match("3")) {
+    number [i] = "Can you be my neighbor?"    
 }
 }
-
+}
 
 $(document).ready(function() {
   $("#form-Uno").submit(function(event) {
@@ -12,6 +15,6 @@ $(document).ready(function() {
    $("form-Uno").submit(function(event) {
     event.preventDefault();
     const inputNumber = parseInt($("#number").val());
-   });
-  });
-});
+   })
+  })
+})
